f1eb78fdd97652839fee4f4fb2616cee24fcbbc7
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-24 20:33:10 +0100

    Design notification messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy'
+    get 'users/sign_out', :to => 'devise/sessions#destroy'
+  end
   resources :courses
   root 'home#index'
 
