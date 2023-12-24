a5f501000aa4e0a2d00167a377fbddc185ff6af5
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-25 00:47:01 +0100

    Styling Courses and home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,8 @@
 class HomeController < ApplicationController
-  def index; end
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    @courses = Course.all.limit(3)
+    @latest_couses = Course.all.limit(3).order(created_at: :desc)
+  end
 end
