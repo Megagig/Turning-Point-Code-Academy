6230f37251bae6c95e16547dfd31f6349b26a783
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-27 12:31:33 +0100

    Add Sidebar search form on the courses page

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,4 +7,22 @@ class Course < ApplicationRecord
   end
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at description id id_value language level price short_description slug title updated_at user_id]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    ['user']
+  end
+
+  LANGUAGES = %i[English Russian Polish Spanish].freeze
+  def self.languages
+    LANGUAGES.map { |language| [language, language] }
+  end
+
+  LEVELS = %i[Beginner Intermediate Advanced].freeze
+  def self.levels
+    LEVELS.map { |level| [level, level] }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,9 @@ class User < ApplicationRecord
   def to_s
     email
   end
+
+  def username
+    email.split(/@/).first
+  end
   has_many :courses
 end
